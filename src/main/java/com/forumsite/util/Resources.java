fbffd04a63d66924c36e27812b2440baac6add50
wanderingivan <angushev.ivan@gmail.com>
2017-02-23 17:58:20 +0200

    Add a basic resources producer

---
+++ src/main/java/com/forumsite/util/Resources.java
@@ -0,0 +1,30 @@
+package com.forumsite.util;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+public class Resources {
+    
+    @PersistenceContext
+    @Produces
+    private EntityManager em;
+
+    @Produces
+    public Logger getLogger(InjectionPoint ip) {
+        String category = ip.getMember().getDeclaringClass().getName();
+        return Logger.getLogger(category);
+    }
+
+    @Produces
+    @RequestScoped
+    public FacesContext getFacesContext() {
+        return FacesContext.getCurrentInstance();
+    }
+}
