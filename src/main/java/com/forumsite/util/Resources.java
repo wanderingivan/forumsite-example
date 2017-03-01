c509300188da9d6c98888ea0bf01ddab47dd3b82
wanderingivan <angushev.ivan@gmail.com>
2017-03-01 20:41:05 +0200

    Add test categories resource.

--- src/main/java/com/forumsite/util/Resources.java
+++ src/main/java/com/forumsite/util/Resources.java
@@ -1,19 +1,24 @@
 package com.forumsite.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 
 public class Resources {
     
-    @PersistenceContext
     @Produces
+    @PersistenceContext
     private EntityManager em;
 
     @Produces
@@ -26,5 +31,15 @@ public class Resources {
     @RequestScoped
     public FacesContext getFacesContext() {
         return FacesContext.getCurrentInstance();
+    }
+    
+    @Named
+    @Produces
+    @ApplicationScoped
+    public List<String> categories(){
+        return Collections.unmodifiableList(Arrays.asList("category1",
+                                                          "category2",
+                                                          "category3",
+                                                          "TestCat"));
     }
 }
