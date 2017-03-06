0b76c93f75515eca4af34b2f51c83bddb44703b6
wanderingivan <angushev.ivan@gmail.com>
2017-03-06 17:08:29 +0200

    Add AbstractWebPageTest - abstract class that defines common graphene test dependencies.

---
+++ src/test/java/com/forumsite/test/web/AbstractWebPageTest.java
@@ -0,0 +1,17 @@
+package com.forumsite.test.web;
+
+import java.net.URL;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.openqa.selenium.WebDriver;
+
+public class AbstractWebPageTest {
+
+    @Drone
+    WebDriver browser;
+    
+    @ArquillianResource
+    URL deploymentUrl;
+    
+}
