c66c721e67bf850f552f2bd6e23a72eec071b1ff
wanderingivan <angushev.ivan@gmail.com>
2017-03-07 22:58:59 +0200

    Add an index controller

---
+++ src/main/java/com/forumsite/web/Index.java
@@ -0,0 +1,29 @@
+package com.forumsite.web;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import com.forumsite.model.ForumThread;
+import com.forumsite.service.ForumThreadProducer;
+
+@Model
+public class Index {
+
+    @Inject
+    private ForumThreadProducer service;
+    
+    private List<ForumThread> threads;
+    
+    @PostConstruct
+    public void init(){
+        threads = service.latest();
+    }
+    
+    public List<ForumThread> getThreads(){
+        return this.threads;
+    }
+    
+}
