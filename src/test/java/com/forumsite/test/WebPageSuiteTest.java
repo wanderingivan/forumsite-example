7eef12836e37fb70398de33a5400896aeb6656b9
wanderingivan <angushev.ivan@gmail.com>
2017-03-11 18:25:05 +0200

    Add a webpage suite test.

---
+++ src/test/java/com/forumsite/test/WebPageSuiteTest.java
@@ -0,0 +1,18 @@
+package com.forumsite.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.forumsite.test.web.*;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    CreateUserPageTests.class,
+    CreateThreadPageTests.class,
+    UpdateThreadPageTests.class,
+    UpdateUserPageTests.class
+})
+public class WebPageSuiteTest {
+
+}
