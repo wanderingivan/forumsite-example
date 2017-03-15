60d961a8bea41b4a84be162ebf0b5a7daab24a82
wanderingivan <angushev.ivan@gmail.com>
2017-03-15 23:48:55 +0200

    Add update comment selenium tests.

---
+++ src/test/java/com/forumsite/test/web/UpdateCommentPageTests.java
@@ -0,0 +1,58 @@
+package com.forumsite.test.web;
+
+import static org.junit.Assert.*;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.graphene.page.InitialPage;
+import org.jboss.arquillian.graphene.page.Page;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.forumsite.test.util.Deployments;
+import com.forumsite.test.web.page.EditCommentPage;
+import com.forumsite.test.web.page.ErrorPage;
+import com.forumsite.test.web.page.LoginPage;
+
+@RunWith(Arquillian.class)
+public class UpdateCommentPageTests extends AbstractWebPageTests {
+
+    
+    @Deployment(testable=false)
+    public static WebArchive createDeployment(){
+        return Deployments.projectWar();
+    }
+    
+    @Page
+    EditCommentPage ePage;
+    
+    @Page
+    ErrorPage errorPage;
+    
+    @Test
+    public void testUpdateComment(@InitialPage LoginPage login){
+        login.loginIfNotAuthenticated("username2", "password");
+        browser.get(deploymentUrl.toExternalForm()+ "/editComment?commentId=1");
+        assertEquals("Editing comment 1",browser.getTitle().trim());
+        ePage.editComment("A new message");
+        
+    }
+    
+    @Test
+    public void testUpdateCommentAccessDeniedFromAcl(@InitialPage LoginPage login){
+        login.loginIfNotAuthenticated("username2", "password");
+        browser.get(deploymentUrl.toExternalForm()+ "/editComment?commentId=1");
+        assertEquals("Editing comment 1",browser.getTitle().trim());
+        ePage.editComment("A new message");
+        errorPage.assertOnAccessDeniedPage();
+    }
+    
+    @Test
+    public void testUpdateCommentAccessDeniedLogin(@InitialPage LoginPage login){
+        login.logoutIfAuthenticated();
+        browser.get(deploymentUrl.toExternalForm()+ "/editComment?commentId=1");       
+        ePage.editComment("A new message");
+        errorPage.assertOnAccessDeniedPage();
+    }
+}
---
+++ src/test/java/com/forumsite/test/web/page/EditCommentPage.java
@@ -0,0 +1,26 @@
+package com.forumsite.test.web.page;
+
+import org.jboss.arquillian.graphene.Graphene;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditCommentPage {
+    
+    @FindBy(id="commentEditForm:message")
+    private WebElement commentMessage;
+    
+    @FindBy(id="commentEditForm:editComment")
+    private WebElement editCommentButton;
+    
+    @FindBy(id="commentEditForm:messageError")
+    private WebElement messageError;
+    
+    public void editComment(String comment){
+        commentMessage.sendKeys(comment);
+        Graphene.guardHttp(editCommentButton).click();
+    }
+
+    public WebElement getMessageError() {
+        return messageError;
+    }
+}
