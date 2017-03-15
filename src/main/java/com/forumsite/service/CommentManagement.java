4d3f2b4c8d4683bd89ec914768e8bdcfbb274586
wanderingivan <angushev.ivan@gmail.com>
2017-03-15 23:30:24 +0200

    Update Comment management interface add update method. Add acl management to CommentManagementImpl.

--- src/main/java/com/forumsite/service/CommentManagement.java
+++ src/main/java/com/forumsite/service/CommentManagement.java
@@ -4,6 +4,10 @@ import com.forumsite.model.Comment;
 
 public interface CommentManagement {
 
+    Comment getComment(long commentId);
+    
     void addComment(Comment comment,String threadName);
+
+    void updateComment(Comment comment);
 
 }
--- src/main/java/com/forumsite/service/impl/CommentManagementImpl.java
+++ src/main/java/com/forumsite/service/impl/CommentManagementImpl.java
@@ -5,26 +5,55 @@ import javax.inject.Inject;
 
 import org.picketlink.Identity;
 import org.picketlink.authorization.annotations.LoggedIn;
+import org.picketlink.http.AccessDeniedException;
+import org.picketlink.idm.PermissionManager;
 import org.picketlink.idm.model.basic.User;
 
 import com.forumsite.data.CommentRepository;
 import com.forumsite.model.Comment;
 import com.forumsite.service.CommentManagement;
+import com.forumsite.util.AuthorizationChecker;
 
 @Stateful
 public class CommentManagementImpl implements CommentManagement {
 
     @Inject
+    private AuthorizationChecker checker;
+    
+    @Inject
     private Identity identity;
     
     @Inject
     private CommentRepository repo;
+
+    @Inject
+    private PermissionManager permissionManager;
     
     @Override
     @LoggedIn
     public void addComment(Comment comment,String threadName) {
         User user = (User) identity.getAccount();
         repo.createComment(comment, threadName, user.getLoginName());
+        createAcl(user,comment);
     }
 
+    @Override
+    public Comment getComment(long commentId) {
+        return repo.retrieveComment(commentId);
+    }
+
+    @Override
+    public void updateComment(Comment comment) {
+        if(!checker.hasApplicationRole("admin") && !this.identity.hasPermission(comment, "update")){
+            throw new AccessDeniedException(String.format("No permission for update on %s by user %s",comment,identity.getAccount()
+                                                                                                                             .getId()));
+        }
+        repo.updateComment(comment.getId(), comment.getMessage());
+        
+    }
+
+    
+    private void createAcl(User user,Comment comment){
+        permissionManager.grantPermission(user, comment, "update, delete");
+    } 
 }
