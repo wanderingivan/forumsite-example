e568fa3b0a095d7decf6c6f9b93f49750ed9b02e
wanderingivan <angushev.ivan@gmail.com>
2017-04-07 22:31:08 +0300

    Add PredicateBuilder single method interface

---
+++ src/main/java/com/forumsite/data/util/PredicateBuilder.java
@@ -0,0 +1,13 @@
+package com.forumsite.data.util;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import com.forumsite.model.Identity;
+
+public interface PredicateBuilder<T extends Identity> {
+
+    Predicate build(CriteriaBuilder cb,Root<T> root,CriteriaQuery<T> query);
+}
