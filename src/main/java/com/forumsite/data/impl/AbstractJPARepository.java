a01e34e7aa5ec7bdbb42d17c28bb822ba13affec
wanderingivan <angushev.ivan@gmail.com>
2017-04-07 22:20:12 +0300

    Add an AbstractJPA Repository implementation.

---
+++ src/main/java/com/forumsite/data/impl/AbstractJPARepository.java
@@ -0,0 +1,85 @@
+package com.forumsite.data.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.forumsite.data.Repository;
+import com.forumsite.data.util.PredicateBuilder;
+import com.forumsite.model.Identity;
+
+public abstract class AbstractJPARepository<T extends Identity> implements Repository<T>{
+
+    protected Class<T> type;
+
+    @Inject
+    private EntityManager em;
+    
+    public AbstractJPARepository(){}
+    
+    public AbstractJPARepository(Class<T> type){
+        this.type = type;
+    }
+    
+    @Override
+    public void add(T t){
+        em.persist(t);
+    }
+    
+    @Override
+    public void update(T t){
+        em.merge(t);
+    }
+    
+    @Override
+    public Optional<T> get(long id){
+        return Optional.ofNullable(em.find(type, id));
+    }
+    
+    protected Optional<T> getWhere(PredicateBuilder<T> pb, EntityGraph<T> graph){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<T> q = cb.createQuery(type);
+        Root<T> root = q.from(type);
+        q.where(pb.build(cb, root,q));
+        try{
+            return Optional.ofNullable(em.createQuery(q)
+                                         .getSingleResult());
+        }catch(NoResultException nre){}
+
+        return Optional.empty();
+    }
+    
+    protected List<T> listWhere(PredicateBuilder<T> pb,int maxResults, EntityGraph<T> graph){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<T> q = cb.createQuery(type);
+        Root<T> root = q.from(type);
+        q.where(pb.build(cb, root,q));
+        TypedQuery<T> tq =  em.createQuery(q);
+        if(maxResults > 0){ tq.setMaxResults(maxResults); }
+        if(graph != null){ tq.setHint("javax.persistence.fetchgraph", graph); }
+        
+        return tq.getResultList();
+    }
+    
+    @Override
+    public void delete(long userId) {
+        em.remove(get(userId).get());
+    } 
+       
+    protected EntityManager em(){
+        return this.em;
+    }
+    
+    protected CriteriaBuilder cb(){
+        return em.getCriteriaBuilder();
+    }
+    
+}
